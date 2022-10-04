088acfd29c70a90c6da05d3237143e94c4a4e4bd
Ruberoid <refafa@yandex.ru>
2022-10-04 23:20:45 +0300

    put.py - work continues

--- put.py
+++ put.py
@@ -1,33 +1,39 @@
 import requests
 
 
-def get_upload_link():
+def get_upload_link(file):
     with open('token.txt', 'r') as opened_file:
         token = opened_file.read()
     headers = {'Authorization': f'OAuth {token}'}
-    params = {'path': 'disk:/Загрузки/govno.txt',
-              'limit': '10',
+    path = f'disk:/Загрузки/{file}'
+    params = {'path': path,
               'overwrite': 'false'}
     url = 'https://cloud-api.yandex.net/v1/disk/resources/upload'
-    r = requests.get(url=url, headers=headers, params=params)
-    k = r.json()
-    print(k)
-    link = k['href']
-    print(link)
-    return link
+    req = requests.get(url=url, headers=headers, params=params)
+    if req.status_code == 200:
+        jsoned_req = req.json()
+        upload_link = jsoned_req['href']
+        return upload_link
+    elif req.status_code == 409:
+        print('Error: file already exists!')
+        return None
+    else:
+        print('Error! Something goes wrong!')
+        return None
 
 
-def upload_file(file, link):
-    with open('token.txt', 'r') as opened_file:
-        token = opened_file.read()
-    headers = {'Authorization': f'OAuth {token}'}
-    params = {'overwrite': 'false'}
-    with open(file, mode='r') as opened_file:
-        t = requests.put(url=link, headers=headers, params=params, data='Gbsdfasdfasdfasdfasdfasdfasdf')
-        print(t)
+def upload_file(file, upload_link):
+    if upload_link is not None:
+        with open('token.txt', 'r') as opened_file:
+            token = opened_file.read()
+        headers = {'Authorization': f'OAuth {token}'}
+        params = {'overwrite': 'false'}
+        with open(file, mode='r') as opened_file:
+            req = requests.put(url=upload_link, headers=headers, params=params, data=opened_file.read())
+        if req.status_code == 201:
+            print('File uploaded successfully!')
+        else:
+            print('Something goes wrong!')
 
-upload_file('test.txt', get_upload_link())
 
-# with open('test.txt', mode='r') as opened_file:
-#     r = requests.put('https://uploader20o.disk.yandex.net:443/upload-target/20221004T144743.459.utd.2i01vbtdzwknsppxyreuxkbnr-k20o.572205', opened_file)
-#     print(r)
+upload_file('test.txt', get_upload_link('test.txt'))
