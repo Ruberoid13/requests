9c43d3b97e0c41e4952de67172105ff1fe104173
Ruberoid <refafa@yandex.ru>
2022-10-11 23:10:08 +0300

    stackoverflow_api.py - work in progress

--- stackoverflow_api.py
+++ stackoverflow_api.py
@@ -4,23 +4,72 @@ import requests
 dt_now = datetime.now()
 
 print(f'Current date and time: {dt_now}')
-time = int(datetime.timestamp(dt_now)) - int(datetime.timestamp(dt_now)) % 86400
+time = int(datetime.timestamp(dt_now))
+time = time - time % 86400
+print(time)
 print(f'Current date and time in seconds from 01.01.1970: {time}')
-url_time = time - 86400 * 2
+time = time - 86400 * 2 - 10800
+print('====================')
+print(datetime.fromtimestamp(time))
+print('====================')
+
+
 
 url = 'https://api.stackexchange.com//2.3/questions'
+page = 1
 headers = {}
 params = {'order': 'desc',
-          'min': url_time,
+          # 'min': time,
           'sort': 'activity',
-          'site': 'stackoverflow'}
-r = requests.get(url=url, params=params, headers=headers)
-print(f'Status code: {r.status_code}')
-jsoned = r.json()
+          'site': 'stackoverflow',
+          'tagged': 'python',
+          # 'creation_date': time,
+          'fromdate': time,
+          'todate': time + 86400,
+          'pagesize': 100,
+          'page': page}
+# r = requests.get(url=url, params=params, headers=headers)
+# print(f'Status code: {r.status_code}')
+# print('====================')
+# jsoned = r.json()
 # print(jsoned)
-for i in jsoned['items']:
-    if 'python' in i['tags']:
-        # print(i)
-        print(f"{i['title']}:")
-        print(f"    {i['creation_date']}")
-        print(f"    {i['link']}")
+# print('====================')
+
+while True:
+    params = {'order': 'desc',
+              # 'min': time,
+              'sort': 'activity',
+              'site': 'stackoverflow',
+              'tagged': 'python',
+              # 'creation_date': time,
+              'fromdate': time,
+              'todate': time + 86400,
+              'pagesize': 100,
+              'page': page}
+    r = requests.get(url=url, params=params, headers=headers)
+    if r.status_code == 200:
+
+        jsoned = r.json()
+        print(jsoned)
+        counter = 1
+        print(f'=======PAGE: {page}')
+        for i in jsoned['items']:
+            # if 'python' in i['tags']:
+                # print(i)
+            print(f"{counter}: {i['title']}:")
+            # print(f"    {datetime.fromtimestamp(i['creation_date'])}")
+            # print(f"    {i['link']}")
+            counter += 1
+        if jsoned['has_more'] == True:
+            print(jsoned['has_more'])
+            page += 1
+
+        else:
+            print(jsoned['has_more'])
+            print('Has No More!!!!!!!!!!!')
+            break
+    else:
+        print('BORODA!!!!!!')
+        print(r.status_code)
+        print(r.text)
+        break
