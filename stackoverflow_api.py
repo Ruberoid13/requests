c603e24a0b3cf78e7a2e4b122950112bd72c6e01
Ruberoid13 <refafa@yandex.ru>
2022-10-12 14:34:05 +0300

    stackoverflow_api.py - works fine

--- stackoverflow_api.py
+++ stackoverflow_api.py
@@ -10,7 +10,8 @@ counter = 1
 print('====================')
 print(f'List of posts created in last two days from: '
       f'{date.fromtimestamp(time - 86400 * 2)} to {date.fromtimestamp(time)}:')
-while True:
+read = True
+while read:
     params = {
               'site': 'stackoverflow',
               'tagged': 'python',
@@ -25,16 +26,21 @@ while True:
         print(f'====================\nPAGE: {page}\n====================')
         for i in jsoned['items']:
             print(f"{counter}: {i['title']}:")
-            print(f"    {datetime.fromtimestamp(i['creation_date'])}")
-            print(f"    {i['link']}")
+            print(f"    Created: {datetime.fromtimestamp(i['creation_date'])}")
+            print(f"    Link:    {i['link']}")
             counter += 1
         if jsoned['has_more'] == True:
             page += 1
         else:
             print(f'====================\nEnd of list\n====================')
-            break
+            read = False
+    elif r.status_code == 400:
+        jsoned = r.json()
+        if jsoned.get('error_id') == 403:
+            print('Error! Looks like its too many pages. Page above 25 requires access token or app key.')
+            read = False
     else:
         print('Something goes wrong!')
-        print(r.status_code)
+        print(f'Status code: {r.status_code}')
         print(r.text)
-        break
+        read = False
